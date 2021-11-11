ac2fe9c4c9c1e608edb0d4308ae59712b20c1124
booleans-oss <ztafade@gmail.com>
2021-11-11 22:56:16 +0200

    fix: eslint fix

--- app/server.js
+++ app/server.js
@@ -1,25 +1,27 @@
-var path = require('path');
-var webpack = require('webpack');
-var express = require('express');
-var config = require('./webpack.config');
+const path = require('path')
+const webpack = require('webpack')
+const express = require('express')
+const config = require('./webpack.config')
 
-var app = express();
-var compiler = webpack(config);
+const app = express()
+const compiler = webpack(config)
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  publicPath: config.output.publicPath
-}));
+app.use(
+  require('webpack-dev-middleware')(compiler, {
+    publicPath: config.output.publicPath
+  })
+)
 
-app.use(require('webpack-hot-middleware')(compiler));
+app.use(require('webpack-hot-middleware')(compiler))
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'index.html'))
+})
 
-app.listen(3000, function(err) {
+app.listen(3000, function (err) {
   if (err) {
-    return console.error(err);
+    return console.error(err)
   }
 
-  console.log('Listening at http://localhost:3000/');
-});
+  console.log('Listening at http://localhost:3000/')
+})
