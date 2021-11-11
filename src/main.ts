d78a2e774e9c818430c89a99dd884972439e8fd7
booleans-oss <ztafade@gmail.com>
2021-11-11 23:37:33 +0200

    fix: switch to pm2 process

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
-async function run(): Promise<void> {
-  // eslint-disable-next-line no-console
-  console.log('test')
+import {getInput, setFailed} from '@actions/core'
+import {command} from 'execa'
+
+async function run(): Promise<string | void> {
+  const startScript = getInput('start')
+  try {
+    const {stdout} = await command(
+      `pm2 start npm --name "pa11y" -- run ${startScript}`
+    )
+    // eslint-disable-next-line no-console
+    console.log(stdout)
+
+    return stdout
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    return setFailed(e.message)
+  }
 }
 
 run()
