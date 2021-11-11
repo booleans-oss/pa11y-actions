9605f690153b756da7f304c47488850f9fbd8feb
booleans-oss <ztafade@gmail.com>
2021-11-11 23:27:35 +0200

    fix: changed to sample function

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,6 @@
-import {getInput, setFailed} from '@actions/core'
-import {command} from 'execa'
-
-async function run(): Promise<string | void> {
-  const startScript = getInput('start')
-  try {
-    const {stdout} = await command(`npm run ${startScript}`)
-    // eslint-disable-next-line no-console
-    console.log(stdout)
-
-    return stdout
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (e: any) {
-    return setFailed(e.message)
-  }
+async function run(): Promise<void> {
+  // eslint-disable-next-line no-console
+  console.log('test')
 }
 
 run()
