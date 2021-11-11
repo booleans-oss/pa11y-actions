cdc100b3171911509bed73c338755d16c5723533
booleans-oss <ztafade@gmail.com>
2021-11-12 00:17:55 +0200

    chore: added full logging

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,20 @@
 import {getInput, setFailed} from '@actions/core'
 import {command} from 'execa'
+import pa11y from 'pa11y'
 
 async function run(): Promise<string | void> {
   const startScript = getInput('start')
   try {
+    // eslint-disable-next-line no-console
+    console.log('before pm2')
     const {stdout} = await command(
       `pm2 start npm --name 'pa11y' -- run ${startScript}`
     )
     // eslint-disable-next-line no-console
-    console.log(stdout)
+    console.log('after pm2')
+    const results = await pa11y('http://localhost:3000')
+    // eslint-disable-next-line no-console
+    console.log(results)
 
     return stdout
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
