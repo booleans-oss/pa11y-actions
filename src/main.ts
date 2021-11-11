e800bd3722006c210abec5816bf51d7e9d2e6cff
booleans-oss <ztafade@gmail.com>
2021-11-11 23:45:06 +0200

    fix: switch to child_process

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,13 @@
 import {getInput, setFailed} from '@actions/core'
-import {command} from 'execa'
+import {exec} from 'child_process'
 
-async function run(): Promise<string | void> {
+async function run(): Promise<void> {
   const startScript = getInput('start')
-  try {
-    const {stdout} = await command(
-      `pm2 start npm --name "pa11y" -- run ${startScript}`
-    )
+  exec(`pm2 start npm --name 'pa11y' -- run ${startScript}`, err => {
+    if (err) return setFailed(err.message)
     // eslint-disable-next-line no-console
-    console.log(stdout)
-
-    return stdout
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (e: any) {
-    // eslint-disable-next-line no-console
-    console.log(e)
-    return setFailed(e.message)
-  }
+    console.log('duh')
+  })
 }
 
 run()
