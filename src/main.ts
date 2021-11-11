c820f52f98af322f1d96723859c03aa3e10fa033
booleans-oss <ztafade@gmail.com>
2021-11-11 23:53:57 +0200

    chore: added pm2 start to workflow

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,6 @@
-import {getInput, setFailed} from '@actions/core'
-import {exec} from 'child_process'
-
 async function run(): Promise<void> {
-  const startScript = getInput('start')
-  exec(`pm2 start npm --name 'pa11y' -- run ${startScript}`, err => {
-    if (err) return setFailed(err.message)
-    // eslint-disable-next-line no-console
-    console.log('duh')
-  })
+  // eslint-disable-next-line no-console
+  console.log('duh')
 }
 
 run()
