b1f96fb11ea59d86325bf8b37d53d6c3c38a8cdb
booleans-oss <ztafade@gmail.com>
2021-11-12 01:03:56 +0200

    fix: using github exec

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import {getInput, setFailed} from '@actions/core'
-import {command} from 'execa'
+import {getExecOutput} from '@actions/exec'
+
 import pa11y from 'pa11y'
 
 async function run(): Promise<string | void> {
@@ -7,22 +8,17 @@ async function run(): Promise<string | void> {
   try {
     // eslint-disable-next-line no-console
     console.log('before pm2')
-    const {stdout} = await command(
-      `pm2 start npm --name 'pa11y' -- run ${startScript}`
-    )
+    await getExecOutput(`pm2 start npm --name 'pa11y' -- run ${startScript}`)
     // eslint-disable-next-line no-console
     console.log('after pm2')
     const results = await pa11y('http://localhost:3000', {
       chromeLaunchConfig: {
-        executablePath:
-          '/home/runner/work/OsirisHackaton21/OsirisHackaton21/node_modules/puppeteer/.local-chromium/linux-869685',
+        executablePath: '/opt/hostedtoolcache/chromium/latest/x64/chrome',
         ignoreHTTPSErrors: false
       }
     })
     // eslint-disable-next-line no-console
     console.log(results)
-
-    return stdout
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   } catch (e: any) {
     // eslint-disable-next-line no-console
