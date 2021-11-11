997be6d99e1d01d3b603464d5018509455b2c630
booleans-oss <ztafade@gmail.com>
2021-11-12 00:04:26 +0200

    fix: added exec script

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,22 @@
-async function run(): Promise<void> {
-  // eslint-disable-next-line no-console
-  console.log('duh')
+import {getInput, setFailed} from '@actions/core'
+import {command} from 'execa'
+
+async function run(): Promise<string | void> {
+  const startScript = getInput('start')
+  try {
+    const {stdout} = await command(
+      `pm2 start npm --name 'pa11y' -- run ${startScript}`
+    )
+    // eslint-disable-next-line no-console
+    console.log(stdout)
+
+    return stdout
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (e: any) {
+    // eslint-disable-next-line no-console
+    console.log(e)
+    return setFailed(e.message)
+  }
 }
 
 run()
